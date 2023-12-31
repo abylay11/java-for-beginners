be1a4f209c54965e62be95f83bd0b17f773973a8
abylay11 <abl_2023@outlook.com>
2023-12-31 18:01:14 +0500

    Lesson 5. For loop in Java.

---
+++ src/lesson5/ForLoop.java
@@ -0,0 +1,15 @@
+package lesson5;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i: " + i);
+        }
+
+        for (int i = 10; i >= 0; i-=5) {
+            System.out.println("i2: " + i);
+        }
+    }
+
+}
