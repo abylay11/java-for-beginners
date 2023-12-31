b45e69894ba5c6b3a3fbf486060bfcfeaa150972
abylay11 <abl_2023@outlook.com>
2023-12-31 18:07:44 +0500

    Lesson 6. Conditional if statement.

---
+++ src/lesson6/If.java
@@ -0,0 +1,16 @@
+package lesson6;
+
+public class If {
+
+    public static void main(String[] args) {
+        int myInt = 15;
+        if (myInt < 10) {
+            System.out.println("1");
+        } else if (myInt > 20) {
+            System.out.println("2");
+        } else {
+            System.out.println("3");
+        }
+    }
+
+}
