f49536cf1ac07a26bf470852371a8a0f66b9c8e0
abylay11 <abl_2023@outlook.com>
2023-12-31 17:15:20 +0500

    Lesson 4. While loop in Java.

---
+++ src/lesson4/WhileLoops.java
@@ -0,0 +1,13 @@
+package lesson4;
+
+public class WhileLoops {
+
+    public static void main(String[] args) {
+        int value = 0;
+        while (value < 5) {
+            System.out.println("Value: " + value);
+            value++;
+        }
+    }
+
+}
