a6c4bf20f41593dfed1454d1c07877332261886c
abylay11 <abl_2023@outlook.com>
2023-12-31 16:45:20 +0500

    Lesson 3. Strings in Java. Reference data types.

---
+++ src/lesson3/Strings.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        int x = 5;
+        String hello = "Hello"; // Hello is an object of String class and hello var is the link to this object
+        String space = " ";
+        String name = "Bob";
+        System.out.println(hello + space + name);
+        System.out.println("Var x equal to - " + x);
+    }
+
+}
