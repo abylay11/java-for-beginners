d04b3712f13dadb72448971b09fd107b03443679
abylay11 <abl_2023@outlook.com>
2024-01-09 15:14:11 +0500

    Lesson 22: The 'final' keyword in Java.

---
+++ src/lesson22/Lesson22.java
@@ -0,0 +1,15 @@
+package lesson22;
+
+public class Lesson22 {
+
+    public static void main(String[] args) {
+        // Lesson 22
+        // Ключевое слово final
+        System.out.println(Test.CONSTANT);
+    }
+
+}
+
+class Test {
+    public static final int CONSTANT = 10;
+}
