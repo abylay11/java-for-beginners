cd5b7695d2685addf90aa111dbcdabf53b4f17c3
abylay11 <abl_2023@outlook.com>
2024-01-10 03:29:04 +0500

    Lesson 31: Wrapper classes of primitive data types.

---
+++ src/lesson31/Test.java
@@ -0,0 +1,10 @@
+package lesson31;
+
+public class Test {
+
+    public static void main(String[] args) {
+        // Double, Float, Long, Integer, Short, Byte, Character, Boolean
+        System.out.println(Integer.parseInt("12313"));
+    }
+
+}
