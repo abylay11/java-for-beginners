c7a66985df5bcfd54f4238c8211d461e8790925f
abylay11 <abl_2023@outlook.com>
2024-01-09 15:35:46 +0500

    Lesson 23: StringBuilder in Java.

---
+++ src/lesson23/Lesson23.java
@@ -0,0 +1,35 @@
+package lesson23;
+
+public class Lesson23 {
+
+    public static void main(String[] args) {
+        // Lesson 23
+        // StringBuilder
+
+        String s = "hello";     // String class objects immutable
+        s = s.toUpperCase();    // method return new object
+        System.out.println("Uppercase: " + s);  // print new object that refs 's' variable
+
+        System.out.println("---");
+
+        String string1 = "Hello";   // create object 1
+        String string2 = " from";   // create object 2
+        String string3 = " Kazakhstan.";    // create object 3
+
+        // string1 + string 2 = new String()
+        // but it is slow than StringBuilder class
+        String allStrings = string1 + string2 + string3;
+
+        System.out.println("All strings: " + allStrings);
+
+        System.out.println("---");
+
+        StringBuilder sb;
+        sb = new StringBuilder("Languages:\n");
+        sb.append("Java\n");
+        sb.append("Kotlin\n").append("Switch\n").append("JavaScript\n");
+
+        System.out.println("String builder - " + sb);
+    }
+
+}
