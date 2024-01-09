6fbdda487262f963f43fd2e105ec9f40586f69c6
abylay11 <abl_2023@outlook.com>
2024-01-10 03:19:14 +0500

    Lesson 30: Casting primitive data types.

---
+++ src/lesson30/Test.java
@@ -0,0 +1,13 @@
+package lesson30;
+
+public class Test {
+    public static void main(String[] args) {
+        // Урок 30: Приведение примитивных типов данных
+        float f = 123.2F;
+        long l = 111L;
+
+        int x = (int)l;     // явное преведение
+        System.out.println(x);
+
+    }
+}
