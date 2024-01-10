6c65a99598852f97b56a200ddb96c8eba74d5e26
abylay11 <abl_2023@outlook.com>
2024-01-10 07:24:52 +0500

    Lesson 34: Wildcards (Generics).

---
+++ src/lesson34/Dog.java
@@ -0,0 +1,4 @@
+package lesson34;
+
+public class Dog extends Animal{
+}
---
+++ src/lesson34/Test.java
@@ -0,0 +1,25 @@
+package lesson34;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Animal> listOfAnimals = new ArrayList<>();
+        listOfAnimals.add(new Animal(1));
+        listOfAnimals.add(new Animal(2));
+
+        List<Dog> listOfDogs = new ArrayList<>();
+        listOfDogs.add(new Dog());
+        listOfDogs.add(new Dog());
+
+        test(listOfAnimals);
+        test(listOfDogs);
+    }
+
+    private static void test(List<? extends Animal> list) {
+        for (Animal animal : list) {
+            animal.eat();
+        }
+    }
+}
